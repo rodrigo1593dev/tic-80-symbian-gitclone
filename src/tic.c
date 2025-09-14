e16564073e30ecb30ee2fa90d978b74d616c6528
rodrigo1593dev <rodrigocaneda77@gmail.com>
2025-09-14 19:03:40 -0300

    Update tic.c

--- src/tic.c
+++ src/tic.c
@@ -9,6 +9,8 @@
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
 
+//compile please
+
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
 
